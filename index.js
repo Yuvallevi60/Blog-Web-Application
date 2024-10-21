f9a31804b1438870617244f2c81702bcfb01a64f
Yuvallevi60 <137276987+Yuvallevi60@users.noreply.github.com>
2024-10-21 23:57:15 +0300

    adding style (not complete)

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-
 const app = express();
 const port = 3000;
 const posts = [];
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
